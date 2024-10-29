b097b183e2dc79aa06a815450a0491641ff54d68
Gereon Kremer <gereon.kremer@gmail.com>
2024-10-29 09:43:18 +0100

    add constant lookup in fib

--- Multisig.sol
+++ Multisig.sol
@@ -5,7 +5,27 @@ import "./State.sol";
 contract Multisig is State {
 
     function fib(uint256 i) pure private returns (uint256) {
-        if (i == 0) return 0;
+        if (i == 0) return 1;
+        if (i == 1) return 1;
+        if (i == 2) return 2;
+        if (i == 3) return 3;
+        if (i == 4) return 5;
+        if (i == 5) return 8;
+        if (i == 6) return 13;
+        if (i == 7) return 21;
+        if (i == 8) return 34;
+        if (i == 9) return 55;
+        if (i == 10) return 89;
+        if (i == 11) return 144;
+        if (i == 12) return 233;
+        if (i == 13) return 377;
+        if (i == 14) return 610;
+        if (i == 15) return 987;
+        if (i == 16) return 1597;
+        if (i == 17) return 2584;
+        if (i == 18) return 4181;
+        if (i == 19) return 6765;
+
         uint256 last = 1;
         uint256 res = 1;
         while (i > 1) {
